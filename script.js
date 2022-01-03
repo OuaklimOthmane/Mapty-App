b0fbd744fc57f6900c9901617a358af926a38ee9
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-01-03 01:47:27 +0100

    Managing Workout Data / create classes

--- script.js
+++ script.js
@@ -14,6 +14,44 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 //! REFACTORING FOR PROJECT ARCHITECTURE :
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // "min/km" :
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // "km/h" :
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
 class App {
   #map;
   #mapEvent;
