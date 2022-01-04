b7ccba9c72454eef51c37d448a5406091e83023a
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-01-04 20:59:23 +0100

    Hide form after a new workout

--- script.js
+++ script.js
@@ -116,6 +116,20 @@ class App {
     inputDistance.focus();
   }
 
+  _hideForm() {
+    //* Clear input fields :
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+
+    //* Hide form :
+    form.style.display = "none";
+    form.classList.add("hidden");
+    setTimeout(() => (form.style.display = "grid"), 1000);
+  }
+
   _toggleElevationField() {
     inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
     inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
@@ -176,12 +190,8 @@ class App {
     //* Render workout in list :
     this._renderWorkoutList(workout);
 
-    //* Hide form & Clear input fields :
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        "";
+    //* Hide form  :
+    this._hideForm();
   }
 
   _renderWorkoutMarker(workout) {
